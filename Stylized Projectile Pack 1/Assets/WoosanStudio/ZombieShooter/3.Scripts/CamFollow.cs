bc5d6860b223a3d0b295c9db8ded7a97741c43e4
ChangKyun Ma <hihiduk119@gmail.com>
2019-11-11 12:24:48 +0900

    일단 빽업

---
+++ Stylized Projectile Pack 1/Assets/WoosanStudio/ZombieShooter/3.Scripts/CamFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//화면에서 목적 Pivot 에 따라 다니게 하기위해 만든 녀석
+
+/// <summary>
+/// Target을 계속 따라 다니는 스크립트
+/// ex) 카메라를 달아두면 카메라를 계속 따라 다님.
+/// </summary>
+public class CamFollow : MonoBehaviour
+{
+    [Header ("[따라다닐 타겟 설정]")]
+    public Transform target;
+    Vector3 gab;
+
+    private void Start()
+    {
+        gab = target.position - transform.position;
+    }
+
+    void Update()
+    {
+        transform.localPosition = target.position - gab;    
+    }
+}
---
+++ Stylized Projectile Pack 1/Assets/WoosanStudio/ZombieShooter/3.Scripts/TestCode3.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode3 : MonoBehaviour
+{
+    public GameObject sampleTown;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Fog() 
+    {
+        RenderSettings.fog = !RenderSettings.fog;
+    }
+
+
+
+    /*void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 200, 150), "포그"))
+        {
+            Fog();
+        }
+
+        if (GUI.Button(new Rect(0, 150, 200, 150), "sample town 1"))
+        {
+            sampleTown.SetActive(!sampleTown.activeSelf);
+        }
+
+        if (GUI.Button(new Rect(0, 300, 200, 150), "포스트 프로세싱"))
+        {
+            Post();
+        }
+    }*/
+}
